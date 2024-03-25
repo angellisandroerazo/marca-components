c61778f6156e433da3cec876323846da00b72944
Angel <angel.lisandro.erazo@gmail.com>
2024-03-25 17:10:35 -0500

    New component added

--- src/components/card.js
+++ src/components/card.js
@@ -1,8 +1,107 @@
-export default{
-    '.card-angel' : {
-        weight: '100px',
-        height: '100px',
-        border: '5px solid #000',
-        backgroundColor: '#4DDC34'
-    }
-}
+export default {
+  ".card-image": {
+    borderRadius: "0.35rem",
+  },
+  ".card-head":{
+    paddingTop: "1rem",
+    paddingLeft: "1rem",
+  },
+  ".card-text":{
+    padding: "1rem",
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "1rem",
+  },
+  ".card-title":{
+    fontSize: "1.25rem",
+    lineHeight: "1.75rem",
+    fontWeight : "700",
+    letterSpacing: "-0.025em",
+  },
+  ".card-description":{
+    fontSize: "1rem",
+    lineHeight: "1.5rem",
+    opacity: "0.7"
+  },
+  ".card-green": {
+    border: "2px solid #257035",
+    borderRadius: "0.5rem",
+  },
+  ".card-red": {
+    border: "2px solid #b22a2a",
+    borderRadius: "0.5rem",
+  },
+  ".card-orange": {
+    border: "2px solid #ea580c",
+    borderRadius: "0.5rem",
+  },
+  ".card-yellow": {
+    border: "2px solid #ca8a04",
+    borderRadius: "0.5rem",
+  },
+  ".card-blue": {
+    border: "2px solid #1e40af",
+    borderRadius: "0.5rem",
+  },
+  ".card-purple": {
+    border: "2px solid #7e22ce",
+    borderRadius: "0.5rem",
+  },
+  ".card-gray": {
+    border: "2px solid #404040",
+    borderRadius: "0.5rem",
+  },
+  ".card-light": {
+    border: "2px solid #d4d4d4",
+    borderRadius: "0.5rem",
+  },
+  ".card-head-green":{
+    padding: "1rem",
+    borderTopLeftRadius: "0.35rem",
+    borderTopRightRadius: "0.35rem",
+    backgroundColor: "#257035",
+  },
+  ".card-head-red":{
+    padding: "1rem",
+    borderTopLeftRadius: "0.35rem",
+    borderTopRightRadius: "0.35rem",
+    backgroundColor: "#b22a2a",
+  },
+  ".card-head-orange":{
+    padding: "1rem",
+    borderTopLeftRadius: "0.35rem",
+    borderTopRightRadius: "0.35rem",
+    backgroundColor: "#ea580c",
+  },
+  ".card-head-yellow":{
+    paddingTop: "1rem",
+    padding: "1rem",
+    borderTopLeftRadius: "0.35rem",
+    borderTopRightRadius: "0.35rem",
+    backgroundColor: "#ca8a04",
+  },
+  ".card-head-blue":{
+    padding: "1rem",
+    borderTopLeftRadius: "0.35rem",
+    borderTopRightRadius: "0.35rem",
+    backgroundColor: "#1e40af",
+  },
+  ".card-head-purple":{
+    padding: "1rem",
+    borderTopLeftRadius: "0.35rem",
+    borderTopRightRadius: "0.35rem",
+    backgroundColor: "#7e22ce",
+  },
+  ".card-head-gray":{
+    padding: "1rem",
+    borderTopLeftRadius: "0.35rem",
+    borderTopRightRadius: "0.35rem",
+    backgroundColor: "#404040",
+  },
+  ".card-head-light":{
+    padding: "1rem",
+    borderTopLeftRadius: "0.35rem",
+    borderTopRightRadius: "0.35rem",
+    backgroundColor: "#d4d4d4",
+  },
+};
