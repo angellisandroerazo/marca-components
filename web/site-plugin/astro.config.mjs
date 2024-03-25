739a82fc348ae7475eb1c3751ce6ae82454da58f
Angel <angel.lisandro.erazo@gmail.com>
2024-03-25 18:22:26 -0500

    Change configuration vercel analytics

--- web/site-plugin/astro.config.mjs
+++ web/site-plugin/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@vercel/analytics';
 
 // https://astro.build/config
 export default defineConfig({
